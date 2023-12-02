fb4b62392fcf3225cf1a529ec09f108d15987595
IT21150098 <it21150098@my.sliit.lk>
2023-12-02 16:47:43 +0530

    filter with status

--- src/business-logics/member-logics.js
+++ src/business-logics/member-logics.js
@@ -1,48 +1,94 @@
-module.exports = function({memberCollection, paymentLogics}){
-    return {
+module.exports = function ({ memberCollection, paymentLogics }) {
+  return {
+    createMember: async function (memberObj) {
+      // let email = memberObj.email.trim()
+      // let password = memberObj.password.trim()
+      // let MemberName = email.split("@")[0]
 
-        createMember:async function (memberObj) {
-            // let email = memberObj.email.trim()
-            // let password = memberObj.password.trim()
-            // let MemberName = email.split("@")[0]
+      return memberCollection.createMember(memberObj);
+    },
 
-                    return memberCollection.createMember(memberObj)
-         
-        },
+    getmemberById: function (memberId) {
+      return memberCollection.getmemberById(memberId);
+    },
 
-        getmemberById: function (memberId){
-            return memberCollection.getmemberById(memberId)
-        },
+    getMemberByEmail: function (email) {
+      return memberCollection.getMemberByEmail(email);
+    },
 
-        getMemberByEmail: function (email){
-            return memberCollection.getMemberByEmail(email)
-        },
+    getAllmembers: async function () {
+      return new Promise(async (resolve, reject) => {
+        let allMemebers = await memberCollection.getAllmembers();
+        for (let i = 0; i < allMemebers.length; i++) {
+          let paymentDetails = await paymentLogics.getPaymentByMemberId(
+            allMemebers[i]._id.valueOf()
+          );
+          allMemebers[i].paymentDetails = paymentDetails;
+        }
+        resolve(allMemebers);
+      });
+    },
+    getMemberStatusMembers: async function (memberApprovalStatus) {
+      console.log(memberApprovalStatus);
+      return new Promise(async (resolve, reject) => {
+        let allMembers = await memberCollection.getAllmembers();
+        // let sanitizedMembers = allMembers.map(({ profilePicture, paymentDetails, password, ...rest }) => rest);
 
-        getAllmembers: async function () {
-            return new Promise(async (resolve, reject) => {
-                let allMemebers = await memberCollection.getAllmembers()
-                for(let i=0; i<allMemebers.length; i++){
-                    let paymentDetails = await paymentLogics.getPaymentByMemberId(allMemebers[i]._id.valueOf())
-                    allMemebers[i].paymentDetails = paymentDetails
-                }
-                resolve(allMemebers)
-            })
+        // Filter members based on memberApprovalStatus
+        let approvedMembers = allMembers.filter(
+          (member) => member.memberApprovalStatus === memberApprovalStatus
+        );
 
-        },
-        updateMemberData: function(memberId,updateData){
-            return memberCollection.updateMemberData(memberId,updateData)
-        },
+        let processedMembers = approvedMembers.map(async (member) => {
+            // Fetch payment details
+            // Return a subset of member data along with payment details
+            return {
+                _id:member._id,
+              firstName: member.firstName,
+              lastName: member.lastName,
+              created_at: member.created_at,
+              passportNumber: member.passportNumber,
+              phoneNumber: member.phoneNumber,
+              email: member.email,
+              membershipCategory:member.membershipCategory,
+              updated_at:member.updated_at,
+              memberApprovalStatus:member.memberApprovalStatus,
+              declinedMessage:member.declinedMessage
+            };
+          });
 
-        changeMemberApproval: function (memberId, memberApprovalStatus, declinedMessage) {
-            if (memberApprovalStatus == 'DECLINED' || memberApprovalStatus == 'REMOVED') {
-                return memberCollection.changeMemberApproval(memberId, memberApprovalStatus, declinedMessage)
-            }
-            else {
-                return memberCollection.changeMemberApproval(memberId, memberApprovalStatus, '')
-            }
-        },
+          processedMembers = await Promise.all(processedMembers);
 
-        
 
-    }
-}
+        resolve(processedMembers);
+      });
+    },
+
+    updateMemberData: function (memberId, updateData) {
+      return memberCollection.updateMemberData(memberId, updateData);
+    },
+
+    changeMemberApproval: function (
+      memberId,
+      memberApprovalStatus,
+      declinedMessage
+    ) {
+      if (
+        memberApprovalStatus == "DECLINED" ||
+        memberApprovalStatus == "REMOVED"
+      ) {
+        return memberCollection.changeMemberApproval(
+          memberId,
+          memberApprovalStatus,
+          declinedMessage
+        );
+      } else {
+        return memberCollection.changeMemberApproval(
+          memberId,
+          memberApprovalStatus,
+          ""
+        );
+      }
+    },
+  };
+};
--- src/controllers/member-controller.js
+++ src/controllers/member-controller.js
@@ -1,106 +1,175 @@
 module.exports = function ({ express, memberLogics, paymentLogics, commons }) {
+  this.expressRouter = new express.Router({ mergeParams: true });
 
-    this.expressRouter = new express.Router({ mergeParams: true })
+  this.expressRouter.post("/", createMember);
+  this.expressRouter.get("/getAllmembers", getAllmembers);
+  this.expressRouter.get(
+    "/getMemberStatusMembers/:memberApprovalStatus",
+    getMemberStatusMembers
+  );
+  this.expressRouter.get("/getEmail", getmemberByEmail);
+  this.expressRouter.get("/:memberId", getmemberById);
+  this.expressRouter.put("/:memberId", updateMemberData);
+  this.expressRouter.put("/memberApproval/:memberId", changeMemberApproval);
+  const mongoose = require("mongoose");
 
-    this.expressRouter.post('/', createMember);
-    this.expressRouter.get('/getAllmembers', getAllmembers);
-    this.expressRouter.get('/getEmail', getmemberByEmail);
-    this.expressRouter.get('/:memberId', getmemberById);
-    this.expressRouter.put('/:memberId', updateMemberData);
-    this.expressRouter.put('/memberApproval/:memberId', changeMemberApproval);
-    const mongoose = require('mongoose');
+  return this.expressRouter;
 
-    return this.expressRouter
+  function createMember(req, res, next) {
+    const { body } = req;
+    let paymentDetails = body.paymentDetails;
 
+    delete body.paymentDetails;
 
-    function createMember(req, res, next) {
-        const { body } = req;
-        let paymentDetails = body.paymentDetails;
+    return memberLogics
+      .createMember(body)
+      .then((result) => {
+        console.log("rrr", result);
+        paymentDetails.memberId = result._id;
 
-        delete body.paymentDetails
-
-        return memberLogics.createMember(body).then((result) => {
-            console.log("rrr", result)
-            paymentDetails.memberId = result._id
-
-            return paymentLogics.createPayment(paymentDetails).then((respos) => {
-                res.send({ "status": "member  Saved Successfully", "result": result });
-            }).catch((err => {
-                return next(commons.errorHandler(err));
-            }))
-
-        }).catch((err => {
+        return paymentLogics
+          .createPayment(paymentDetails)
+          .then((respos) => {
+            res.send({ status: "member  Saved Successfully", result: result });
+          })
+          .catch((err) => {
             return next(commons.errorHandler(err));
-        }))
+          });
+      })
+      .catch((err) => {
+        return next(commons.errorHandler(err));
+      });
+  }
+
+  function getmemberByEmail(req, res, next) {
+    const {
+      query: { email },
+    } = req;
+    console.log("email", email);
+    memberLogics
+      .getMemberByEmail(email)
+      .then((result) => {
+        res.send({ result: result });
+      })
+      .catch((err) => {
+        return next(commons.errorHandler(err));
+      });
+  }
+
+  function getmemberById(req, res, next) {
+    const {
+      params: { memberId },
+    } = req;
+
+    if (!mongoose.Types.ObjectId.isValid(memberId)) {
+      return res.status(400).send({ error: "Invalid memberId" });
     }
 
-    function getmemberByEmail(req, res, next) {  
-        const {query:{ email }} = req
-        console.log("email",email)
-        memberLogics.getMemberByEmail(email).then(result => {
-            res.send({ "result": result })
-        }).catch((err => {
-            return next(commons.errorHandler(err));
-        }))
-    }
+    memberLogics
+      .getmemberById(memberId)
+      .then((result) => {
+        res.send({ result: result });
+      })
+      .catch((err) => {
+        return next(commons.errorHandler(err));
+      });
+  }
 
-    function getmemberById(req, res, next) {
-        const { params: { memberId } } = req;
-    
-        if (!mongoose.Types.ObjectId.isValid(memberId)) {
-            return res.status(400).send({ error: 'Invalid memberId' });
-        }
-    
-        memberLogics.getmemberById(memberId)
-            .then(result => {
-                res.send({ "result": result });
-            })
-            .catch(err => {
-                return next(commons.errorHandler(err));
-            });
-    }
+  function getAllmembers(req, res, next) {
+    memberLogics
+      .getAllmembers()
+      .then((result) => {
+        res.send({ result: result });
+      })
+      .catch((err) => {
+        return next(commons.errorHandler(err));
+      });
+  }
+  function getMemberStatusMembers(req, res, next) {
+    const {
+      params: { memberApprovalStatus },
+    } = req;
+    let memberApprovalStatusCAP = memberApprovalStatus.toUpperCase();
+    // const { body: { memberApprovalStatus } } = req
+    // console.log(memberApprovalStatus,"meber-controller- line 73");
+    memberLogics
+      .getMemberStatusMembers(memberApprovalStatusCAP)
+      .then((result) => {
+        res.send({ result: result });
+      })
+      .catch((err) => {
+        return next(commons.errorHandler(err));
+      });
+  }
 
-    function getAllmembers(req, res, next) {
-        memberLogics.getAllmembers().then(result => {
-            res.send({ "result": result })
-        }).catch((err => {
-            return next(commons.errorHandler(err));
-        }))
-    }
+  function updateMemberData(req, res, next) {
+    const {
+      params: { memberId },
+    } = req;
+    const { body } = req;
+    memberLogics
+      .updateMemberData(memberId, body)
+      .then((result) => {
+        res.send({ result: result });
+      })
+      .catch((err) => {
+        return next(commons.errorHandler(err));
+      });
+  }
 
-    function updateMemberData(req, res, next) {
-        const { params: { memberId } } = req
-        const { body } = req
-        memberLogics.updateMemberData(memberId, body).then(result => {
-            res.send({ "result": result })
-        }).catch((err => {
-            return next(commons.errorHandler(err));
-        }))
-    }
+  function changeMemberApproval(req, res, next) {
+    const {
+      params: { memberId },
+    } = req;
+    const {
+      body: { memberApprovalStatus, declinedMessage },
+    } = req;
 
-    function changeMemberApproval(req, res, next) {
-        const { params: { memberId } } = req
-        const { body: { memberApprovalStatus, declinedMessage } } = req
-
-        if (memberApprovalStatus !== 'DECLINED' && memberApprovalStatus !== 'APPROVED' && memberApprovalStatus !== 'PENDING' && memberApprovalStatus !== 'REMOVED') {
-            res.status(400).send({ result: 'Member Approval Status in wrong. It should be APPROVED or PENDING or DECLINED or REMOVED' })
-        }
-        else if ((memberApprovalStatus == 'DECLINED' || memberApprovalStatus == 'REMOVED') && (!declinedMessage || declinedMessage == '')) {
-            res.status(400).send({ result: 'Declined memberships should be have a declined message' })
-        }
-        else if ((memberApprovalStatus == 'DECLINED' || memberApprovalStatus == 'REMOVED') && declinedMessage && declinedMessage != '') {
-            memberLogics.changeMemberApproval(memberId, memberApprovalStatus, declinedMessage).then(result => {
-                res.send({ "result": result })
-            }).catch((err => {
-                return next(commons.errorHandler(err));
-            }))
-        }
-        else {
-            memberLogics.changeMemberApproval(memberId, memberApprovalStatus).then(result => {
-                res.send({ "result": result })
-            }).catch((err => {
-                return next(commons.errorHandler(err));
-            }))
-        }
+    if (
+      memberApprovalStatus !== "DECLINED" &&
+      memberApprovalStatus !== "APPROVED" &&
+      memberApprovalStatus !== "PENDING" &&
+      memberApprovalStatus !== "REMOVED"
+    ) {
+      res
+        .status(400)
+        .send({
+          result:
+            "Member Approval Status in wrong. It should be APPROVED or PENDING or DECLINED or REMOVED",
+        });
+    } else if (
+      (memberApprovalStatus == "DECLINED" ||
+        memberApprovalStatus == "REMOVED") &&
+      (!declinedMessage || declinedMessage == "")
+    ) {
+      res
+        .status(400)
+        .send({
+          result: "Declined memberships should be have a declined message",
+        });
+    } else if (
+      (memberApprovalStatus == "DECLINED" ||
+        memberApprovalStatus == "REMOVED") &&
+      declinedMessage &&
+      declinedMessage != ""
+    ) {
+      memberLogics
+        .changeMemberApproval(memberId, memberApprovalStatus, declinedMessage)
+        .then((result) => {
+          res.send({ result: result });
+        })
+        .catch((err) => {
+          return next(commons.errorHandler(err));
+        });
+    } else {
+      memberLogics
+        .changeMemberApproval(memberId, memberApprovalStatus)
+        .then((result) => {
+          res.send({ result: result });
+        })
+        .catch((err) => {
+          return next(commons.errorHandler(err));
+        });
     }
-}
+  }
+};
